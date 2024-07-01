667a79eb5aabbc98d5d8d7e6f24208b173d3cc5a
Átilo Domingos <113414336+Atilodev@users.noreply.github.com>
2024-07-01 17:39:32 -0300

    Cadastro tecnico de manutenção

---
+++ conecta.php
@@ -0,0 +1,15 @@
+<?php
+    $servername = "localhost";
+    $database = "projeto";
+    $username = "root";
+    $password = "";
+
+
+    $conn = mysqli_connect($servername, $username, $password, $database);
+
+    if (!$conn) {
+        die("Conexão falhou. Erro: " . mysqli_connect_error());
+    }
+
+
+  ?>
